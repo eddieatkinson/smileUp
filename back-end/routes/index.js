773b070a410dde54be16fe6a977507bd489dd124
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-03-19 15:51:18 -0400

    backend hit

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('You hit it /!!');
   res.render('index', { title: 'Express' });
 });
 
--- front-end/src/containers/Home.js
+++ front-end/src/containers/Home.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Home extends Component {
+  buttonClicked() {
+    console.log('Clicked!');
+    const url = `${window.apiHost}`;
+    axios.get(url);
+  }
+
   render() {
     return (
       <div>
         <p>Home</p>
         <a href='/signup'>To SignUp</a>
+        <div>
+          <button onClick={this.buttonClicked}>Test the backend!</button>
+        </div>
       </div>
     )
   }
