34363a9ae257e65fe5fca6ec15fcb04dd14c4d62
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-03-23 14:16:53 -0400

    volunteer sign up form to database

---
+++ front-end/src/actions/SignUpAction.js
@@ -0,0 +1,15 @@
+import { SIGN_UP_ACTION } from "../types";
+import axios from 'axios';
+
+export default function(input) {
+  console.log(input);
+  const axiosPromise = axios({
+    url: `${window.apiHost}/signup`,
+    method: 'POST',
+    data: input,
+  });
+  return {
+    type: SIGN_UP_ACTION,
+    payload: axiosPromise,
+  }
+}
--- front-end/src/containers/SignUp.js
+++ front-end/src/containers/SignUp.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { emailCheck } from '../utilities';
+import SignUpAction from '../actions/SignUpAction';
 
 class SignUp extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    birthday: '',
+    email: '',
+    phone: '',
+    zip: '',
+  }
+  handleFieldChange(event, field) {
+    const { value } = event.target;
+    this.setState({
+      [field]: value,
+    });
+  }
+  handleSubmit(event) {
+    const { firstName, lastName, birthday, email, zip } = this.state;
+    event.preventDefault();
+    if (firstName === '' || lastName === '' || birthday === '' || zip === '') {
+      alert('First name, last name, birthday, and zip are required.');
+    } else if (!email.match(emailCheck)) {
+      alert('Please enter a valid email.');
+    } else {
+      this.props.SignUpAction(this.state);
+    }
+  }
   render() {
     return (
       <div>
         <p>SignUp</p>
         <a href='/'>To Home</a>
+        <form>
+          <input onChange={(event) => this.handleFieldChange(event, 'firstName')} type='text' placeholder='First Name' />
+          <input onChange={(event) => this.handleFieldChange(event, 'lastName')} type='text' placeholder='Last Name' />
+          <input onChange={(event) => this.handleFieldChange(event, 'birthday')} type='date' placeholder='Birthday' />
+          <input onChange={(event) => this.handleFieldChange(event, 'email')} type='email' placeholder='Email' />
+          <input onChange={(event) => this.handleFieldChange(event, 'phone')} type='tel' placeholder='Phone' />
+          <input onChange={(event) => this.handleFieldChange(event, 'zip')} type='text' placeholder='Zip code' />
+          <button onClick={this.handleSubmit.bind(this)} type='submit'>Sign me up!</button>
+        </form>
       </div>
     )
   }
 }
 
-export default SignUp;
+export default connect(null, {
+  SignUpAction,
+})(SignUp);
