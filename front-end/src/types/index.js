a56030078a1ac9cb564395c5de8ce5b7299ed418
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-03-26 18:32:11 -0400

    table reads from database

--- front-end/src/types/index.js
+++ front-end/src/types/index.js
@@ -1,3 +1,4 @@
 export const LOGIN_ACTION = 'LOGIN_ACTION';
 export const REGISTER_ACTION = 'REGISTER_ACTION';
 export const SIGN_UP_ACTION = 'SIGN_UP_ACTION'; // volunteer sign-up
+export const GET_VOLUNTEER_INFO = 'GET_VOLUNTEER_INFO';
