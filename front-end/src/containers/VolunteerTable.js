d386573f04aa1bae635aa05a96f022ad34ea807d
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-03-25 21:49:28 -0400

    data table works

--- front-end/src/containers/VolunteerTable.js
+++ front-end/src/containers/VolunteerTable.js
@@ -3,7 +3,22 @@ import Table from '@material-ui/core/Table';
 import { TableHead, TableRow, TableCell, Paper, TableBody } from '@material-ui/core';
 
 const rows = [
-  {}
+  {
+    id: 1,
+    firstName: 'Eddie',
+    lastName: 'Atkinson',
+    birthday: '08-05-1984',
+    email: 'eddiebatkinson@gmail.com',
+    phone: '770-627-4776',
+  },
+  {
+    id: 2,
+    firstName: 'Crystal',
+    lastName: 'Taylor',
+    birthday: '09-23-1983',
+    email: 'crystaleddie@gmail.com',
+    phone: '352-870-0690',
+  },
 ]
 
 class VolunteerTable extends Component {
@@ -23,7 +38,15 @@ class VolunteerTable extends Component {
               </TableRow>
             </TableHead>
             <TableBody>
-
+              {rows.map(row => (
+                <TableRow key={row.id}>
+                  <TableCell>{row.firstName}</TableCell>
+                  <TableCell>{row.lastName}</TableCell>
+                  <TableCell>{row.birthday}</TableCell>
+                  <TableCell>{row.email}</TableCell>
+                  <TableCell>{row.phone}</TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
         </Paper>
