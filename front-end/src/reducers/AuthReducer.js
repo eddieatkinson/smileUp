3f9b76c082e08ff09caacac5c628ab7a60239a0b
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-03-30 22:25:03 -0400

    login hits database and returns token

--- front-end/src/reducers/AuthReducer.js
+++ front-end/src/reducers/AuthReducer.js
@@ -1,10 +1,12 @@
-import { LOGIN_ACTION, REGISTER_ACTION } from '../types';
+import { LOGIN_ACTION, REGISTER_ACTION, SIGN_IN_ACTION } from '../types';
 
 export default function(state = [], action) {
   switch(action.type) {
     case LOGIN_ACTION:
       return action.payload;
     case REGISTER_ACTION:
+      return action.payload;
+    case SIGN_IN_ACTION:
       return action.payload;
     default:
       return state;
