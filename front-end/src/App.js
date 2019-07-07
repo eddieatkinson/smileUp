e4f0946226b4523b9bb91a58118018ff90f07906
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-07-06 21:48:41 -0400

    finished donate page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -11,6 +11,8 @@ import VolunteerTable from './containers/VolunteerTable';
 import SignIn from './containers/SignIn';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Donate from './components/Donate';
+import Sponsor from './components/Sponsor';
 
 class App extends Component {
 
@@ -27,6 +29,16 @@ class App extends Component {
           <Route path='/volunteers' component={VolunteerTable} />
           <Route path='/facebook' component={() => { 
             window.location.href = 'https://www.facebook.com/smileupfoundation'; 
+            return null;
+          }}/>
+          <Route path='/donate' component={Donate} />
+          <Route path='/sponsor' component={Sponsor} />
+          <Route path='/paypal' component={() => {
+            window.location.href = 'https://www.paypal.com/us/fundraiser/charity/1851712';
+            return null;
+          }}/>
+          <Route path='/amazon' component={() => {
+            window.location.href = 'https://smile.amazon.com/ch/58-2396294';
             return null;
           }}/>
           <Footer />
--- front-end/src/components/Dropdown.js
+++ front-end/src/components/Dropdown.js
@@ -31,9 +31,9 @@ function Dropdown() {
     <div ref={node} style={{position: "relative"}}>
       <div className='give-back-dropdown' style={styles.links} onClick={handleClick}>How to Give Back</div>
       <div id='give-back-items'>
-        <Link className='dropdown-items' to='/'><div>Donate</div></Link>
-        <Link className='dropdown-items' to='/'><div>Volunteer</div></Link>
-        <Link className='dropdown-items' to='/'><div>Sponsor</div></Link>
+        <Link className='dropdown-items' to='/donate'><div>Donate</div></Link>
+        <Link className='dropdown-items' to='/signup'><div>Volunteer</div></Link>
+        <Link className='dropdown-items' to='/sponsor'><div>Sponsor</div></Link>
       </div>
     </div>
   );
