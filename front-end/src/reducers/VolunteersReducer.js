cf8f48ab7676b7225bcef7f217a2d90602bde515
Eddie Atkinson <eddiebatkinson@gmail.com>
2020-02-07 10:23:33 -0500

    added display column for hours, changed css

--- front-end/src/reducers/VolunteersReducer.js
+++ front-end/src/reducers/VolunteersReducer.js
@@ -1,8 +1,13 @@
-import { GET_VOLUNTEER_INFO, DELETE_VOLUNTEER_ACTION, SIGN_UP_ACTION } from '../types';
+import {
+  GET_VOLUNTEER_INFO,
+  DELETE_VOLUNTEER_ACTION,
+  SIGN_UP_ACTION
+} from "../types";
 
-export default function(state=[], action) {
-  switch(action.type) {
+export default function(state = [], action) {
+  switch (action.type) {
     case GET_VOLUNTEER_INFO:
+      console.log(action.payload.data);
       return action.payload.data;
     case DELETE_VOLUNTEER_ACTION:
       return action.payload.data;
